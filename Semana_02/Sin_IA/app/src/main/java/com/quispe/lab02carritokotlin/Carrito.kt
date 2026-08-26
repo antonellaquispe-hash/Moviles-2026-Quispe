7d20378f9fe5baf79542ff8dd4e300b542e44f07
antonellaquispe-hash <antonella.quispe@tecsup.edu.pe>
2026-08-25 23:56:35 -0500

    Agrega data class Producto y variables del carrito

--- Semana_02/Sin_IA/app/src/main/java/com/quispe/lab02carritokotlin/Carrito.kt
+++ Semana_02/Sin_IA/app/src/main/java/com/quispe/lab02carritokotlin/Carrito.kt
@@ -1,8 +1,30 @@
 package com.quispe.lab02carritokotlin
 
+data class Producto(
+    val nombre: String,
+    val precio: Double,
+    var cantidad: Int
+)
+
 fun main() {
-    // 1. Menú inicial (Parte 1)
+
     println("=========================================")
     println("   CARRITO DE COMPRAS - TIENDA TECSUP")
     println("=========================================")
+
+    val nombreCliente = "Juan Leon"
+    val carrito = mutableListOf<Producto>()
+
+    println("Cliente: $nombreCliente")
+    println()
+
+    // 3. Productos del carrito
+    carrito.add(Producto("Laptop HP", 2500.0, 1))
+    carrito.add(Producto("Mouse Logitech", 45.5, 2))
+    carrito.add(Producto("Teclado Logitech", 120.0, 1))
+    carrito.add(Producto("Monitor LG", 850.0, 1))
+
+    for (producto in carrito) {
+        println("Producto agregado: ${producto.nombre}")
+    }
 }
