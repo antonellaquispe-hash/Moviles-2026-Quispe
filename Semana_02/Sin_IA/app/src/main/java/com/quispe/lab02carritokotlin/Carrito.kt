a35c47d24a64b0d0340388a1f7596fe854df46aa
antonellaquispe-hash <antonella.quispe@tecsup.edu.pe>
2026-08-25 23:37:25 -0500

    Proyecto inicial con el titulo

---
+++ Semana_02/Sin_IA/app/src/main/java/com/quispe/lab02carritokotlin/Carrito.kt
@@ -0,0 +1,8 @@
+package com.quispe.lab02carritokotlin
+
+fun main() {
+    // 1. Menú inicial (Parte 1)
+    println("=========================================")
+    println("   CARRITO DE COMPRAS - TIENDA TECSUP")
+    println("=========================================")
+}
