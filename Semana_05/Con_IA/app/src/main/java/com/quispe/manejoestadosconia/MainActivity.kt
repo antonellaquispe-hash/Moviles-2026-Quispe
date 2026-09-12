f181aae4b84516c310312adb8939ac3833560e63
antonellaquispe-hash <antonella.quispe@tecsup.edu.pe>
2026-09-12 18:52:31 -0500

    COMMIT 02: Implementa registro y listado de tareas con POO

--- Semana_05/Con_IA/app/src/main/java/com/quispe/manejoestadosconia/MainActivity.kt
+++ Semana_05/Con_IA/app/src/main/java/com/quispe/manejoestadosconia/MainActivity.kt
@@ -7,13 +7,16 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
-import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
@@ -29,18 +32,55 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun TaskScreen() {
+    var tarea by remember { mutableStateOf("") }
     var tareas by remember { mutableStateOf(listOf<Tarea>()) }
 
     Column(
         modifier = Modifier
             .fillMaxSize()
-            .padding(24.dp),
-        verticalArrangement = Arrangement.Top
+            .padding(24.dp)
     ) {
         Text(text = "Lista de Tareas")
 
         Spacer(modifier = Modifier.height(16.dp))
 
+        OutlinedTextField(
+            value = tarea,
+            onValueChange = { tarea = it },
+            modifier = Modifier.fillMaxWidth(),
+            label = {
+                Text("Escribe una tarea")
+            }
+        )
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Button(
+            onClick = {
+                if (tarea.isNotBlank()) {
+                    tareas = tareas + Tarea(tarea)
+                    tarea = ""
+                }
+            },
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text("Agregar tarea")
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         Text(text = "Cantidad de tareas: ${tareas.size}")
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Column(
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            tareas.forEachIndexed { index, item ->
+                Text(
+                    text = "${index + 1}. ${item.nombre}"
+                )
+            }
+        }
     }
 }
