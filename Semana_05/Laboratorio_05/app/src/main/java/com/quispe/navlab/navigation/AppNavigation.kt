0d259186cf954f677f98b34b533149cf166b281b
antonellaquispe-hash <antonella.quispe@tecsup.edu.pe>
2026-09-21 21:57:01 -0500

    COMMIT 02: Configura navegación principal

---
+++ Semana_05/Laboratorio_05/app/src/main/java/com/quispe/navlab/navigation/AppNavigation.kt
@@ -0,0 +1,28 @@
+package com.quispe.navlab.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = Screen.Home.route
+    ) {
+        composable(Screen.Home.route) {
+        }
+
+        composable(Screen.List.route) {
+        }
+
+        composable(Screen.Detail.route) {
+        }
+
+        composable(Screen.Profile.route) {
+        }
+    }
+}
